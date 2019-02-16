fddeddf29666da928ac5d1dd1eb4a7af04fb553b
Nick Vincent <the.nick.vincent@gmail.com>
2019-02-15 18:50:41 -0800

    Prettify all the things

--- js/config.js
+++ js/config.js
@@ -1,47 +1,47 @@
 // Extension events
-var XRAY_ON      = 'XRAY_ON',
-    XRAY_OFF     = 'XRAY_OFF',
-    UPDATE_ID    = 'UPDATE_ID';
+var XRAY_ON = "XRAY_ON",
+  XRAY_OFF = "XRAY_OFF",
+  UPDATE_ID = "UPDATE_ID";
 
 // Icon paths
-var ICON_ON      = 'img/icon-on.png',
-    ICON_OFF     = 'img/icon-off.png';
+var ICON_ON = "img/icon-on.png",
+  ICON_OFF = "img/icon-off.png";
 
 // Options alerts
-var MSG_SUCCESS  = 'Your changes have been saved.',
-    MSG_ERROR    = 'Oops, there was an error saving your changes.',
-    MSG_RESET    = 'Your settings have been reset to the defaults.',
-    MSG_TIMOUT   = 5000;
+var MSG_SUCCESS = "Your changes have been saved.",
+  MSG_ERROR = "Oops, there was an error saving your changes.",
+  MSG_RESET = "Your settings have been reset to the defaults.",
+  MSG_TIMOUT = 5000;
 
 // Default values
 var DEFAULTS = {
-  'style-tag-id'        : '___dom-x-ray___',
-  'root-selector'       : 'html',
+  "style-tag-id": "___dom-x-ray___",
+  "root-selector": "html",
 
-  'include-background'  : true,
-  'background-color'    : '#70BBFF',
-  'background-opacity'  : 0.03,
+  "include-background": true,
+  "background-color": "#70BBFF",
+  "background-opacity": 0.03,
 
-  'include-outline'     : true,
-  'outline-color'       : '#FF69B4',
-  'outline-opacity'     : 0.25,
+  "include-outline": true,
+  "outline-color": "#FF69B4",
+  "outline-opacity": 0.25,
 
-  'include-text'        : true,
-  'text-color'          : '#FF69B4',
-  'text-opacity'        : 1.00,
+  "include-text": true,
+  "text-color": "#FF69B4",
+  "text-opacity": 1.0,
 
-  'include-border'      : true,
-  'border-color'        : '#FF69B4',
-  'border-opacity'      : 1.00,
+  "include-border": true,
+  "border-color": "#FF69B4",
+  "border-opacity": 1.0,
 
-  'include-images'      : true,
-  'image-opacity'       : 0.80,
+  "include-images": true,
+  "image-opacity": 0.8,
 
-  'include-root'        : true,
-  'include-descendants' : true,
-  'include-pseudo'      : true,
-  'use-important'       : true,
+  "include-root": true,
+  "include-descendants": true,
+  "include-pseudo": true,
+  "use-important": true,
 
   // Flag to save defaults to storage on first init
-  'first-init'          : true
+  "first-init": true
 };
--- js/options.js
+++ js/options.js
@@ -2,69 +2,71 @@
 var options = {};
 
 // Generated CSS
-var xRayCss = '';
+var xRayCss = "";
 
 // Demo ID
-var demoSelector = '#demo';
+var demoSelector = "#demo";
 
 // Get elements
-var form         = document.getElementById('options-form'),
-    inputs       = form.getElementsByTagName('input'),
-    textInputs   = form.querySelectorAll('input[type=text]');
-    statusMsg    = document.getElementById('status');
+var form = document.getElementById("options-form"),
+  inputs = form.getElementsByTagName("input"),
+  textInputs = form.querySelectorAll("input[type=text]"),
+  statusMsg = document.getElementById("status");
 
 // Timeout for hiding status
 var statusTimeout = null;
 
 // Load the options & init
-loadOptions( init );
+loadOptions(init);
 
 // Initialize the options page
-function init( loadedOptions ) {
-  console.log('Initializing DOM X-Ray options...');
+function init(loadedOptions) {
+  console.log("Initializing DOM X-Ray options...");
   options = loadedOptions;
   updateInputs();
   updateDemo();
   addEventHandlers();
-  console.log('DOM X-Ray options Initialized!');
+  console.log("DOM X-Ray options Initialized!");
 }
 
 // Update the input values
 function updateInputs() {
-  console.log('Updating inputs...');
-  for ( var i = 0; i < inputs.length; i++ ) {
+  console.log("Updating inputs...");
+  for (var i = 0; i < inputs.length; i++) {
     var input = inputs[i];
-    setValue( input );
-    updateLabel( input );
+    setValue(input);
+    updateLabel(input);
   }
 }
 
 // Update the demo styles
 function updateDemo() {
-  console.log('Updating demo...');
+  console.log("Updating demo...");
 
   // clone options object so we can override root-selector for the demo
-  var demoOptions = JSON.parse( JSON.stringify( options ) );
-  demoOptions['root-selector'] = demoSelector;
-  xRayCss = generateCss( demoOptions );
+  var demoOptions = JSON.parse(JSON.stringify(options));
+  demoOptions["root-selector"] = demoSelector;
+  xRayCss = generateCss(demoOptions);
 
-  injectStyles( demoOptions['style-tag-id'], xRayCss );
+  injectStyles(demoOptions["style-tag-id"], xRayCss);
 }
 
 // Add the event handlers
 function addEventHandlers() {
-  console.log('Adding event handlers...');
+  console.log("Adding event handlers...");
 
   // Input change handlers
-  for ( var i = 0; i < inputs.length; i++ ) {
+  for (var i = 0; i < inputs.length; i++) {
     var input = inputs[i];
     input.onchange = onInputChange;
 
     // Add autoselect if it's a text input
-    if ( input.type === 'text' ) {
+    if (input.type === "text") {
       input.onfocus = onInputFocus;
       input.onblur = onInputBlur;
-      input.onmouseup = function( e ) { e.preventDefault(); };
+      input.onmouseup = function(e) {
+        e.preventDefault();
+      };
     }
   }
 
@@ -77,104 +79,109 @@ function addEventHandlers() {
 function onInputChange() {
   var input = this;
   var key = input.id;
-  var value = getValue( input );
+  var value = getValue(input);
   options[key] = value;
-  updateLabel( input );
+  updateLabel(input);
   updateDemo();
 }
 
 // When an text input gets focus & blurs
 var focused = null;
 function onInputFocus() {
-  if ( focused === this ) return;
+  if (focused === this) return;
   focused = this;
-  setTimeout( function () { focused.select(); }, 50 );
+  setTimeout(function() {
+    focused.select();
+  }, 50);
 }
 function onInputBlur() {
-  if ( focused === this ) focused = null;
+  if (focused === this) focused = null;
   this.value = this.value.trim();
 }
 
 // When the save button is clicked
-function onSave( e ) {
+function onSave(e) {
   e.preventDefault();
-  saveOptions( options,
+  saveOptions(
+    options,
     function() {
-      updateStatus( MSG_SUCCESS, 'success' );
+      updateStatus(MSG_SUCCESS, "success");
     },
     function() {
-      updateStatus( MSG_ERROR, 'error' );
+      updateStatus(MSG_ERROR, "error");
     }
   );
 }
 
 // When the reset button is clicked
-function onReset( e ) {
+function onReset(e) {
   e.preventDefault();
-  options = JSON.parse( JSON.stringify( DEFAULTS ) );
+  options = JSON.parse(JSON.stringify(DEFAULTS));
   updateInputs();
   updateDemo();
 
-  saveOptions( options,
+  saveOptions(
+    options,
     function() {
-      updateStatus( MSG_RESET, 'success' );
+      updateStatus(MSG_RESET, "success");
     },
     function() {
-      updateStatus( MSG_ERROR, 'error' );
+      updateStatus(MSG_ERROR, "error");
     }
   );
 }
 
 // Display a status message
-function updateStatus( msg, state ) {
+function updateStatus(msg, state) {
   statusMsg.textContent = msg;
-  statusMsg.classList.remove('hidden');
-  if ( state ) statusMsg.classList.add( state );
-  clearTimeout( statusTimeout );
+  statusMsg.classList.remove("hidden");
+  if (state) statusMsg.classList.add(state);
+  clearTimeout(statusTimeout);
   statusTimeout = setTimeout(function() {
-    statusMsg.classList.add('hidden');
-  }, MSG_TIMOUT );
+    statusMsg.classList.add("hidden");
+  }, MSG_TIMOUT);
 }
 
 // Input helper: set value
-function setValue( input ) {
+function setValue(input) {
   var key = input.id;
-  if ( input.type === 'checkbox' ) {
+  if (input.type === "checkbox") {
     input.checked = options[key] ? true : false;
-  }
-  else {
+  } else {
     input.value = options[key];
   }
 }
 
 // Input helper: get value
-function getValue( input ) {
-  if ( input.type === 'checkbox' ) {
+function getValue(input) {
+  if (input.type === "checkbox") {
     return input.checked ? true : false;
   }
-  if ( input.type === 'range' ) {
-    return parseFloat( input.value );
+  if (input.type === "range") {
+    return parseFloat(input.value);
   }
-  if ( input.id === 'style-tag-id' && input.value.indexOf('#') === 0 ) {
-    console.log( input.value.split('#')[1].trim() )
-    return input.value.split('#')[1].trim();
+  if (input.id === "style-tag-id" && input.value.indexOf("#") === 0) {
+    console.log(input.value.split("#")[1].trim());
+    return input.value.split("#")[1].trim();
   }
   return input.value.trim();
 }
 
 // Update labels for color selector inputs
-function updateLabel( input ) {
-  if ( input.type === 'color' || input.type === 'range' ) {
+function updateLabel(input) {
+  if (input.type === "color" || input.type === "range") {
     var id = input.id;
     var value = input.value;
-    var labelVal = document.querySelector( 'label[for=' + id + '] .value' );
-    if ( input.type === 'range' ) value = parseInt( value * 100 ) + '%';
+    var labelVal = document.querySelector("label[for=" + id + "] .value");
+    if (input.type === "range") value = parseInt(value * 100) + "%";
     labelVal.textContent = value;
-  }
-  else if ( input.type === 'checkbox' && input.parentNode.nodeName.toLowerCase() === 'legend' ) {
+  } else if (
+    input.type === "checkbox" &&
+    input.parentNode.nodeName.toLowerCase() === "legend"
+  ) {
     var fieldset = input.parentNode.parentNode;
-    if ( input.checked ) fieldset.classList.add( 'enabled' );
-    else fieldset.classList.remove( 'enabled' );
+    if (input.checked) fieldset.classList.add("enabled");
+    else fieldset.classList.remove("enabled");
   }
 }
 
